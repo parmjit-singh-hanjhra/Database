b30fd6e98f32fecea4f623cfaaac62569f5c163e
Parmjit Singh <parmjit.singh@mail.utoronto.ca>
2018-11-18 21:54:33 -0500

    Modified q1 and q2(has an error with latest_election_won view)

--- table_definitions/q1.sql
+++ table_definitions/q1.sql
@@ -15,10 +15,29 @@ partyName VARCHAR(100)
 
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
-DROP VIEW IF EXISTS intermediate_step CASCADE;
+-- DROP VIEW IF EXISTS intermediate_step CASCADE;
 
 -- Define views for your intermediate steps here.
+CREATE VIEW election_year_format AS
+SELECT CAST(DATE_PART('YEAR', e.e_date) AS INT) AS e_date, country_id, id, votes_valid 
+FROM election e;
 
+CREATE FUNCTION range(v real) RETURNS VARCHAR(20) AS $$
+BEGIN 
+	IF (v > 0  AND v <= 5) THEN RETURN '(0,5]';
+	ELSIF (v > 5  AND v <= 10) THEN RETURN '(5,10]'; 
+	ELSIF (v > 10  AND v <= 20) THEN RETURN '(10,20]';
+	ELSIF (v > 20  AND v <= 30) THEN RETURN '(20,30]';
+	ELSIF (v > 30  AND v <= 40) THEN RETURN '(30,40]';
+	ELSIF (v > 40  AND v <= 100) THEN RETURN '(40,100]';
+	ELSE RETURN NULL;
+	END IF;
+END $$ LANGUAGE plpgsql;
+
+SELECT c.name, p.name_short, e.e_date, range(avg(e_r.votes * 100.0 / e.votes_valid)) 
+FROM country c, election_year_format e, election_result e_r, party p
+WHERE (c.id = e.country_id) AND (e.id = e_r.election_id) AND (e_r.party_id = p.id) AND (e.e_date BETWEEN 1996 AND 2016) and (c.name = 'Germany')
+GROUP BY c.name, p.name_short, e.e_date;
 
 -- the answer to the query 
 insert into q1 
--- table_definitions/q2.sql
+++ table_definitions/q2.sql
@@ -17,12 +17,52 @@ mostRecentlyWonElectionYear INT
 
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
-DROP VIEW IF EXISTS intermediate_step CASCADE;
+-- DROP VIEW IF EXISTS intermediate_step CASCADE;
 
 -- Define views for your intermediate steps here.
+
+CREATE VIEW max_votes AS
+SELECT c.name AS country_name, e.id AS election_id, max(e_r.votes) AS votes
+FROM election_result e_r, election e, country c
+WHERE (e_r.election_id = e.id) AND (e.country_id = c.id)
+GROUP BY c.name, e.id;
+
+CREATE VIEW winning_parties AS
+SELECT m_v.country_name AS country_name, e_r.party_id AS party_id, count(*) AS num_wins 
+FROM max_votes m_v, election_result e_r
+WHERE (m_v.election_id =  e_r.election_id) AND (m_v.votes = e_r.votes)
+GROUP BY m_v.country_name, e_r.party_id;
+
+CREATE VIEW avg_wins AS
+SELECT country_name , avg(num_wins) AS avg_wins
+FROM winning_parties w_p
+GROUP BY country_name;
+
+CREATE VIEW best_avg_parties AS
+SELECT w_p.country_name AS country_name, w_p.party_id AS party_id, w_p.num_wins AS num_wins
+FROM winning_parties w_p, avg_wins a_w 
+WHERE (w_p.country_name = a_w.country_name) AND (w_p.num_wins > (3*a_w.avg_wins));
+
+CREATE VIEW latest_election_won AS
+SELECT c.name AS country_name, e_r.party_id AS party_id, max(e.e_date) AS most_recent_date
+FROM max_votes m_v, election e, election_result e_r, country c
+WHERE (m_v.election_id = e.id) AND (e.id = e_r.election_id) AND (e_r.votes = m_v.votes) AND (c.id = e.country_id)
+GROUP e_r.party_id;
+
+
+
+//SELECT l_e_w.country_name AS countryName, p.name AS partyName, p_f.family AS partyFamily, b_a_p.num_wins AS wonElections,
+//	   l_e_w.election_id AS mostRecentlyWonElectionId, CAST(DATE_PART('YEAR', l_e_w.most_recent_date) AS INT)  
+//FROM (((latest_election_won l_e_w JOIN best_avg_parties b_a_p ON  (l_e_w.party_id = b_a_p.party_id)) JOIN 
+//party p ON (l_e_w.party_id = p.id)) LEFT JOIN party_family p_f ON (l_e_w.party_id = p_f.party_id));
+ 
+
+
+
 
 
 -- the answer to the query 
 insert into q2 
+
 
 
