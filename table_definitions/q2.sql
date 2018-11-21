1b120665b15e0508aeaf46b5a7058a3d3ce0f24b
Parmjit Singh <parmjit.singh@mail.utoronto.ca>
2018-11-20 22:06:05 -0500

    updated q2 and q3

--- table_definitions/q2.sql
+++ table_definitions/q2.sql
@@ -17,52 +17,59 @@ mostRecentlyWonElectionYear INT
 
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
--- DROP VIEW IF EXISTS intermediate_step CASCADE;
+DROP VIEW IF EXISTS max_votes CASCADE;
+DROP VIEW IF EXISTS winning_parties CASCADE;
+DROP VIEW IF EXISTS avg_wins CASCADE;
+DROP VIEW IF EXISTS best_avg_parties CASCADE;
+
 
 -- Define views for your intermediate steps here.
 
+-- Get the elections of a country with maximum votes
 CREATE VIEW max_votes AS
 SELECT c.name AS country_name, e.id AS election_id, max(e_r.votes) AS votes
 FROM election_result e_r, election e, country c
 WHERE (e_r.election_id = e.id) AND (e.country_id = c.id)
 GROUP BY c.name, e.id;
 
+-- Matches the party with the maximum votes from max_votes and counts the number of win of that party for each country
 CREATE VIEW winning_parties AS
 SELECT m_v.country_name AS country_name, e_r.party_id AS party_id, count(*) AS num_wins 
 FROM max_votes m_v, election_result e_r
 WHERE (m_v.election_id =  e_r.election_id) AND (m_v.votes = e_r.votes)
 GROUP BY m_v.country_name, e_r.party_id;
 
+-- Gets the average number of elections won per party at a given country
 CREATE VIEW avg_wins AS
 SELECT country_name , avg(num_wins) AS avg_wins
 FROM winning_parties w_p
 GROUP BY country_name;
 
+-- Retrieve the parties that have won more than three times the average number of elections won by the parties
+-- in the same country
 CREATE VIEW best_avg_parties AS
-SELECT w_p.country_name AS country_name, w_p.party_id AS party_id, w_p.num_wins AS num_wins
-FROM winning_parties w_p, avg_wins a_w 
-WHERE (w_p.country_name = a_w.country_name) AND (w_p.num_wins > (3*a_w.avg_wins));
+SELECT w_p.country_name AS country_name, w_p.party_id AS party_id, w_p.num_wins AS num_wins, p_f.family AS family
+FROM (winning_parties w_p JOIN avg_wins a_w ON  (w_p.country_name = a_w.country_name)) LEFT JOIN party_family p_f ON (p_f.party_id = w_p.party_id)
+WHERE (w_p.num_wins > (3*a_w.avg_wins));
 
-CREATE VIEW latest_election_won AS
+-- Retrieves the latest election of the winning best_avg
+CREATE VIEW latest_years AS
 SELECT c.name AS country_name, e_r.party_id AS party_id, max(e.e_date) AS most_recent_date
 FROM max_votes m_v, election e, election_result e_r, country c
 WHERE (m_v.election_id = e.id) AND (e.id = e_r.election_id) AND (e_r.votes = m_v.votes) AND (c.id = e.country_id)
-GROUP e_r.party_id;
+GROUP BY e_r.party_id, c.name;
 
-
-
-//SELECT l_e_w.country_name AS countryName, p.name AS partyName, p_f.family AS partyFamily, b_a_p.num_wins AS wonElections,
-//	   l_e_w.election_id AS mostRecentlyWonElectionId, CAST(DATE_PART('YEAR', l_e_w.most_recent_date) AS INT)  
-//FROM (((latest_election_won l_e_w JOIN best_avg_parties b_a_p ON  (l_e_w.party_id = b_a_p.party_id)) JOIN 
-//party p ON (l_e_w.party_id = p.id)) LEFT JOIN party_family p_f ON (l_e_w.party_id = p_f.party_id));
- 
-
-
-
-
+CREATE VIEW latest_election_won AS
+SELECT l_y.country_name AS country_name, l_y.party_id AS party_id, l_y.most_recent_date AS most_recent_date, e.id AS election_id  
+FROM latest_years l_y, country c, election e
+WHERE (l_y.country_name = c.name) AND (e.country_id = c.id) AND (l_y.most_recent_date = e.e_date);
 
 -- the answer to the query 
 insert into q2 
 
+SELECT l_e_w.country_name AS countryName, p.name AS partyName, b_a_p.family AS partyFamily, b_a_p.num_wins AS wonElections,
+	   l_e_w.election_id AS mostRecentlyWonElectionId, CAST(DATE_PART('YEAR', l_e_w.most_recent_date) AS INT) AS mostRecentlyWonElectionYear  
+FROM ((latest_election_won l_e_w JOIN best_avg_parties b_a_p ON  (l_e_w.party_id = b_a_p.party_id)) JOIN 
+party p ON (l_e_w.party_id = p.id));
 
 
--- table_definitions/q3.sql
+++ table_definitions/q3.sql
@@ -13,11 +13,50 @@ create table q3(
 
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
-DROP VIEW IF EXISTS intermediate_step CASCADE;
+DROP VIEW IF EXISTS avg_participation_rate CASCADE;
+DROP VIEW IF EXISTS valid_candidates CASCADE;
+DROP VIEW IF EXISTS resulting_countries_from_a_p_r CASCADE;
+DROP VIEW IF EXISTS resulting_countries_from_v_c CASCADE;
+DROP VIEW IF EXISTS final_countries CASCADE;
 
 -- Define views for your intermediate steps here.
 
+-- This view creates the table grouped by with countryName, year and average participation rate
+CREATE VIEW avg_participation_rate AS
+SELECT country_id, EXTRACT(YEAR FROM e_date) AS year, COALESCE(AVG(votes_cast * 100.0 / electorate), 0) AS avg_participation_rate 
+FROM election e
+WHERE (EXTRACT(YEAR FROM e_date) BETWEEN 2001 AND 2016) AND electorate IS NOT NULL 
+GROUP BY country_id, year;
 
+-- From this view we get the country names with the condition of Y1 < Y2 and avg rate >-= to previouse avg rate (But it considers just pairs not all)
+CREATE VIEW valid_candidates AS
+SELECT country_id, year, avg_participation_rate
+FROM avg_participation_rate a_p_r1
+WHERE a_p_r1.avg_participation_rate >= ALL (SELECT a_p_r2.avg_participation_rate
+                                            FROM avg_participation_rate a_p_r2 
+                                            WHERE a_p_r1.country_id = a_p_r2.country_id AND a_p_r1.year >= a_p_r2.year);
+
+CREATE VIEW resulting_countries_from_a_p_r AS 
+SELECT country_id, count(year) AS num_years
+FROM avg_participation_rate a_p_r
+GROUP BY country_id;
+
+CREATE VIEW resulting_countries_from_v_c AS
+SELECT country_id, count(year) AS num_years
+FROM valid_candidates
+GROUP BY country_id;
+
+CREATE VIEW final_countries AS
+SELECT r_c_v_c.country_id AS country_id
+FROM resulting_countries_from_v_c r_c_v_c, resulting_countries_from_a_p_r r_c_a_p_r
+WHERE r_c_v_c.num_years = r_c_a_p_r.num_years AND r_c_a_p_r.country_id = r_c_v_c.country_id;
+ 
 -- the answer to the query 
 insert into q3 
+
+SELECT c.name AS countryName, a_p_r.year AS year, a_p_r.avg_participation_rate AS participationRatio
+FROM final_countries f_c, avg_participation_rate a_p_r ,  country c
+WHERE f_c.country_id = a_p_r.country_id AND c.id = a_p_r.country_id; 
+
+
 
