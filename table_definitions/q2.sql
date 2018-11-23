08490f9850c91d0134d2c69d16f9f064071f0e59
Parmjit Singh <parmjit.singh@mail.utoronto.ca>
2018-11-22 22:31:09 -0500

    modified q2.sql to change average output

--- table_definitions/q2.sql
+++ table_definitions/q2.sql
@@ -14,62 +14,79 @@ mostRecentlyWonElectionId INT,
 mostRecentlyWonElectionYear INT
 );
 
-
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
 DROP VIEW IF EXISTS max_votes CASCADE;
 DROP VIEW IF EXISTS winning_parties CASCADE;
-DROP VIEW IF EXISTS avg_wins CASCADE;
-DROP VIEW IF EXISTS best_avg_parties CASCADE;
+DROP VIEW IF EXISTS num_country_party CASCADE;
+DROP VIEW IF EXISTS sum_winning_parties CASCADE;
+DROP VIEW IF EXIST\qS avg_wins CASCADE;
+DROP VIEW IF EXISTS candidate_parties CASCADE;
 DROP VIEW IF EXISTS latest_years CASCADE;
 DROP VIEW IF EXISTS latest_election_won CASCADE;
+
+
 -- Define views for your intermediate steps here.
 
--- Get the elections of a country with maximum votes
-CREATE VIEW max_votes AS
-SELECT c.name AS country_name, e.id AS election_id, max(e_r.votes) AS votes
-FROM election_result e_r, election e, country c
-WHERE (e_r.election_id = e.id) AND (e.country_id = c.id)
-GROUP BY c.name, e.id;
+-- get maximum number of votes for a country at a given election
+CREATE VIEW max_votes AS 
+SELECT e.country_id AS country_id, e.id AS election_id, MAX(e_r.votes) AS max_votes
+FROM election e, election_result e_r
+WHERE e.id = e_r.election_id
+GROUP BY e.country_id, e.id; 
 
--- Matches the party with the maximum votes from max_votes and counts the number of win of that party for each country
+-- Match the maximum votes from max_votes to the parties' votes and this results in winning parties 
 CREATE VIEW winning_parties AS
-SELECT m_v.country_name AS country_name, e_r.party_id AS party_id, count(*) AS num_wins 
-FROM max_votes m_v, election_result e_r
-WHERE (m_v.election_id =  e_r.election_id) AND (m_v.votes = e_r.votes)
-GROUP BY m_v.country_name, e_r.party_id;
+SELECT m_v.country_id AS country_id, e_r.party_id AS party_id, count(*) AS win_nums
+FROM max_votes m_v, election_result e_r 
+WHERE (m_v.election_id = e_r.election_id) AND (m_v.max_votes = e_r.votes)
+GROUP BY m_v.country_id, e_r.party_id;
 
--- Gets the average number of elections won per party at a given country
-CREATE VIEW avg_wins AS
-SELECT country_name , avg(num_wins) AS avg_wins
+-- Gets the number of election run per party in a country
+CREATE VIEW num_country_party AS
+SELECT e.country_id AS country_id, count(DISTINCT e_r.party_id) AS party_count
+FROM election_result e_r, election e
+WHERE e_r.election_id = e.id
+GROUP BY e.country_id;
+
+-- Sum the winnings of all winning parties in a country
+CREATE VIEW sum_winning_parties AS 
+SELECT w_p.country_id AS country_id, SUM(win_nums) AS sum_wins
 FROM winning_parties w_p
-GROUP BY country_name;
+GROUP BY w_p.country_id;
 
--- Retrieve the parties that have won more than three times the average number of elections won by the parties
--- in the same country
-CREATE VIEW best_avg_parties AS
-SELECT w_p.country_name AS country_name, w_p.party_id AS party_id, w_p.num_wins AS num_wins, p_f.family AS family
-FROM (winning_parties w_p JOIN avg_wins a_w ON  (w_p.country_name = a_w.country_name)) LEFT JOIN party_family p_f ON (p_f.party_id = w_p.party_id)
-WHERE (w_p.num_wins > (3*a_w.avg_wins));
+-- Average of winning parties by sum of winning parties divided by the number of parties in a country
+CREATE VIEW avg_wins AS 
+SELECT n_c_p.country_id AS country_id, s_w_p.sum_wins / n_c_p.party_count AS avg_wins
+FROM num_country_party n_c_p, sum_winning_parties s_w_p 
+WHERE n_c_p.country_id = s_w_p.country_id;
 
--- Retrieves the latest election of the winning best_avg
+-- Gets all the winning parties that are greater than 3 times the average winning parties
+CREATE VIEW candidate_parties AS
+SELECT w_p.country_id AS country_id, w_p.party_id AS party_id, w_p.win_nums AS win_nums, p_f.family AS family
+FROM (winning_parties w_p JOIN avg_wins a_w  ON (w_p.country_id = a_w.country_id)) LEFT JOIN party_family p_f ON (p_f.party_id = w_p.party_id)
+WHERE (w_p.win_nums > (3 * a_w.avg_wins)); 
+
+-- Retrieves the latest election year of the winning best average parties
 CREATE VIEW latest_years AS
-SELECT c.name AS country_name, e_r.party_id AS party_id, max(e.e_date) AS most_recent_date
-FROM max_votes m_v, election e, election_result e_r, country c
-WHERE (m_v.election_id = e.id) AND (e.id = e_r.election_id) AND (e_r.votes = m_v.votes) AND (c.id = e.country_id)
-GROUP BY e_r.party_id, c.name;
+SELECT m_v.country_id AS country_id, e_r.party_id AS party_id, max(e.e_date) AS most_recent_date
+FROM max_votes m_v, election e, election_result e_r
+WHERE (m_v.election_id = e.id) AND (m_v.election_id = e_r.election_id) AND (e_r.votes = m_v.max_votes)
+GROUP BY m_v.country_id, e_r.party_id;
 
+-- Retrieves the latest election of the winning best average parties 
 CREATE VIEW latest_election_won AS
-SELECT l_y.country_name AS country_name, l_y.party_id AS party_id, l_y.most_recent_date AS most_recent_date, e.id AS election_id  
-FROM latest_years l_y, country c, election e
-WHERE (l_y.country_name = c.name) AND (e.country_id = c.id) AND (l_y.most_recent_date = e.e_date);
+SELECT l_y.country_id AS country_id, l_y.party_id AS party_id, l_y.most_recent_date AS most_recent_date, e.id AS election_id  
+FROM latest_years l_y, election e
+WHERE (l_y.most_recent_date = e.e_date) AND (l_y.country_id = e.country_id);
+
 
 -- the answer to the query 
 insert into q2 
 
-SELECT l_e_w.country_name AS countryName, p.name AS partyName, b_a_p.family AS partyFamily, b_a_p.num_wins AS wonElections,
-	   l_e_w.election_id AS mostRecentlyWonElectionId, CAST(DATE_PART('YEAR', l_e_w.most_recent_date) AS INT) AS mostRecentlyWonElectionYear  
-FROM ((latest_election_won l_e_w JOIN best_avg_parties b_a_p ON  (l_e_w.party_id = b_a_p.party_id)) JOIN 
-party p ON (l_e_w.party_id = p.id));
+SELECT c.name AS countryName, p.name AS partyName, c_p.family AS partyFamily, c_p.win_nums AS wonElections,
+           l_e_w.election_id AS mostRecentlyWonElectionId, CAST(DATE_PART('YEAR', l_e_w.most_recent_date) AS INT) AS mostRecentlyWonElectionYear  
+FROM ((latest_election_won l_e_w JOIN candidate_parties c_p ON  (l_e_w.party_id = c_p.party_id) JOIN country c ON (c.id = l_e_w.country_id)) JOIN party p ON (l_e_w.party_id = p.id));
+
 
 
